45b39d08713460fb2f9b9397bb80515c245156b4
JooergenKoeldal <joer1894@stud.kea.dk>
2021-02-04 11:44:15 +0100

    added names

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
 
+    //Lavet af Max Meldal, Rasmus Berg, Thomas Luff og Jørgen Køldal
+
     public static void main(String[] args) {
 	Simulation simulation = new Simulation(2);
 	simulation.start();
